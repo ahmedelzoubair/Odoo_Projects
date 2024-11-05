7bed36c93ec8db0ec3a79c1f97d280e70080d0ca
Ahmed Sayed El Zoubair <35544320+ahmedelzoubair@users.noreply.github.com>
2024-11-05 16:14:21 +0200

    Add field at SO and bill & delete option access

---
+++ version 15/om_odoo_inheriteence/models/conf_user_at_so_line.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order.line'
+
+    confirmed_user_id_soLine = fields.Many2one('res.users',string="Confirmed User",related='order_id.confirmed_user_id',store=True)
+    # Note:- 1st of all to make a relation between two models we need to create same field category (if the the two field must be same field type)
+    # related='order_id.confirmed_user_id' :- There is a relation between two model by default [order_id = fields.Many2one('sale.order', string='Order Reference', ondelete='cascade')]
+    # so the (order_id) is the relation filed between two model
