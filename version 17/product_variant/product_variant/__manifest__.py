4f0acbf8942268b9a08c57a414eb189cc48d1113
Ahmed Sayed El Zoubair <35544320+ahmedelzoubair@users.noreply.github.com>
2025-10-20 14:02:34 +0300

    Separate product price from product variables

---
+++ version 17/product_variant/product_variant/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Product Variant',
+    'version': '17.0.1.0.0',
+    'summary': '',
+    'description': 'This model is separating product price than product variant price (lst price)',
+    'author': 'Capstone Solutions',
+    'website': 'https://www.capstone-solution.com',
+    'sequence': -100,
+    'license': 'LGPL-3',
+    'depends': ['sale', 'stock'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
